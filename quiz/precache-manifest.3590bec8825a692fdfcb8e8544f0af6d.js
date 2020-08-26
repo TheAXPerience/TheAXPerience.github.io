d629f96de98c600147a9f6057d023c488527e583
Alvin <alvin3xu1@gmail.com>
2020-08-26 15:49:34 -0700

    new version

---
+++ quiz/precache-manifest.3590bec8825a692fdfcb8e8544f0af6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae8a332a60600c50bdff6b0f145ca1f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "548465a277970ed13135",
+    "url": "/static/css/main.52ce1e10.chunk.css"
+  },
+  {
+    "revision": "480e747b1dec88edfcd3",
+    "url": "/static/js/2.9aa96267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9aa96267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "548465a277970ed13135",
+    "url": "/static/js/main.002bb741.chunk.js"
+  },
+  {
+    "revision": "4b36be1e5bf5734e29b7",
+    "url": "/static/js/runtime-main.0f8ea258.js"
+  }
+]);
